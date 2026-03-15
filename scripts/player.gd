55978f6c0dce6afe5d8d64d6ebbf630bdc76ae58
SidSam15 <siddhant.samar@gmail.com>
2026-03-15 10:20:54 -0500

    camera

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 @export var speed = 100
-@export var jump_velocity = -300
+@export var jump_velocity = -250
 @export var gravity = 980
 @export var max_jumps = 2
 @export var break_force = 750
