d155b1452c969c052497fb0235354d53c018bdf2
Adtrn <narayanan.aditya0@gmail.com>
2026-03-15 20:33:45 -0500

    half of boss stuff

--- scenes/breakable_roof.gd
+++ scenes/breakable_roof.gd
@@ -1,7 +1,18 @@
 extends StaticBody2D
 
+@onready var trigger = $FallTrigger
+
 func break_roof():
 	queue_free()
+
+	# remove collision so player falls through
+	$CollisionShape2D.disabled = true
+
+	# enable fall trigger
+	trigger.monitoring = true
+
+	# hide the roof
+	$Sprite2D.visible = false
 
 
 # Called when the node enters the scene tree for the first time.
@@ -12,3 +23,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_fall_trigger_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
